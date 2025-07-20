d8bb464ca0561a6b76dfe9946f641cdca04e8ca7
ishand3thrx <ishanb129@gmail.com>
2025-07-20 15:44:00 +0530

    remove console calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-
-console.log('client side js is loaded')
-
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
